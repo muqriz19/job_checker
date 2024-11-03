ac75cc1dc1814603479b68779b326ccc24970107
muqriz19 <muqriz19othman@gmail.com>
2024-11-03 11:27:45 +0800

    commit

--- app/be/obj/Debug/net8.0/be.AssemblyInfo.cs
+++ app/be/obj/Debug/net8.0/be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b15f914b83fa15c90427d7318fd31762b7b4635")]
 [assembly: System.Reflection.AssemblyProductAttribute("be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
